2363cca772d418bc0646cf939a6529e67cfa0756
renee <renee.xu321@gmail.com>
2025-09-21 20:54:02 -0400

    changes

--- src/robot/planner/include/planner_node.hpp
+++ src/robot/planner/include/planner_node.hpp
@@ -4,6 +4,16 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "planner_core.hpp"
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <optional>
 
 class PlannerNode : public rclcpp::Node {
   public:
